4eba7912d17529a9d2f7e49794221bc06c45884d
Enrique <enrique.j.matta@gmail.com>
2022-02-19 10:39:21 -0600

    stubs

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import Phaser from "phaser";
+import BootScene from "./scene/BootScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: [BootScene],
+};
+
+export default config;
